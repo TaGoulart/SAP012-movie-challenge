139f98c3d6c6666a00c6a6cc5e4a418ff09cf7b6
TaGoulart <tamara.bgg@gmail.com>
2024-04-05 11:51:28 -0300

    criando os card

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-import App from './components/App.js';
+import { App } from './components/App.js'; // Importe o componente App corretamente
 
+// Adicione o componente App ao elemento com id 'root'
 document.getElementById('root').appendChild(App());
-
-
-
-
